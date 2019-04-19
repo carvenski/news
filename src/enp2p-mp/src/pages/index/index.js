ad9dd5c7184b85f6bd40f647b650a5ac8991b057
yxzoro <yangxing20107@gmail.com>
2019-04-19 11:42:43 +0800

    .

--- src/enp2p-mp/src/pages/index/index.js
+++ src/enp2p-mp/src/pages/index/index.js
@@ -10,26 +10,34 @@ export default class Index extends Component {
 
   componentWillMount () { }
 
+  // 页面加载后开始执行js逻辑
   componentDidMount () { 
-    console.log("==========")
-
+    // 判断是否之前登录过
     Taro.getSetting({
       complete: function (res) {
-        console.log("=> 用户是否登陆", res.authSetting["scope.userInfo"])
+        is_login = res.authSetting["scope.userInfo"]
+        console.log("is_login = ", is_login)
+        if (is_login) {
+          // 已登录,获取user info
+          Taro.getUserInfo({
+            scope: "scope.userInfo",
+            complete: function (res) {
+              console.log("=> got user info:");
+              console.log(res);
+            }
+          });
+        } else {
+          // 未登录,提示授权
+          Taro.showToast({
+            title: '请您先授权',
+            icon: 'success',
+            duration: 3000,
+            mask: true
+          }).then(res => console.log(res))
+        }
       }
     });
-
-    Taro.getUserInfo({
-      scope: "scope.userInfo",
-      complete: function (res) {
-        console.log(res);
-      }
-
-    });
-
-
   }
-
 
   componentWillUnmount () { }
 
@@ -41,7 +49,7 @@ export default class Index extends Component {
     return (
       <View>
         <Text>Please Login First.</Text>
-        <button open-type="getUserInfo">请您授权</button>
+        <button open-type="getUserInfo">授权按钮</button>
       </View>
     )
   }
